462818c2f9c9453c1aeb5c439e9f7d2f3e03855d
若安 <3172683060@qq.com>
2026-08-09 17:37:29 +0800

    Revert "使用Repository secrets"
    This reverts commit f9ee2f7e0de1c9f07e1cedecb0b9abeced7db312.

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = 'SUPABASE_URL';  // 替换为你的 Project URL
-const SUPABASE_ANON_KEY = 'SUPABASE_ANON_KEY';  // 替换为你的 anon key
+const SUPABASE_URL = 'https://ovwylpfgfwgqqfquirnx.supabase.co';  // 替换为你的 Project URL
+const SUPABASE_ANON_KEY = 'sb_publishable_wSRNnQqJ-6k0WJ_0XsZubg_NyMjPxTP';  // 替换为你的 anon key
 
 const API_URL = SUPABASE_URL + '/rest/v1';
 const HEADERS = {
